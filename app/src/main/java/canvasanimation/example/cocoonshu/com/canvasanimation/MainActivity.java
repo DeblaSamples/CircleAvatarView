0efe0de905572048b295c7d694d78d8579a10f4e
Cocoonshu <shujian@oppo.com>
2016-05-16 14:45:45 +0800

    update
         update comments

--- app/src/main/java/canvasanimation/example/cocoonshu/com/canvasanimation/MainActivity.java
+++ app/src/main/java/canvasanimation/example/cocoonshu/com/canvasanimation/MainActivity.java
@@ -18,6 +18,12 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
+/**
+ * AvatarView
+ * @Auther Cocoonshu
+ * @Date 2016-05-16 10:02
+ * Copyright (c) 2016 Cocoonshu
+ */
 public class MainActivity extends AppCompatActivity {
 
     private String               mStrFilePath  = null;
